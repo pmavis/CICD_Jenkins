96318588dacd21957f4f801e3eb5c08bddeffae3
pmavis <77163522+pmavis@users.noreply.github.com>
2023-05-31 10:48:54 +0200

    Create JavierApplication.java

---
+++ ejercicio4/src/main/java/com/example/javier/javier/JavierApplication.java
@@ -0,0 +1,13 @@
+package com.example.javier.javier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavierApplication.class, args);
+	}
+
+}
