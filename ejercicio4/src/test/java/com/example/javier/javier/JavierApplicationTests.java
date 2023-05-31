03fa695a62390b6636467c415266d1cec31a1a6d
pmavis <77163522+pmavis@users.noreply.github.com>
2023-05-31 10:50:29 +0200

    Create JavierApplicationTests.java

---
+++ ejercicio4/src/test/java/com/example/javier/javier/JavierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javier.javier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
