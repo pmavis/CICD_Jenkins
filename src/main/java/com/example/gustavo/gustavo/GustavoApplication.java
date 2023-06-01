c5ff5e395051de44e0b9c4a73d734c3d31a53cdb
pmavis <77163522+pmavis@users.noreply.github.com>
2023-06-01 10:20:08 +0200

    Add files via upload

---
+++ src/main/java/com/example/gustavo/gustavo/GustavoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gustavo.gustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustavoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GustavoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gustavo/gustavo/GustavoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gustavo.gustavo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
